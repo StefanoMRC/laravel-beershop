0dae0e04d4b7d09902db578c54dcf14a477528c1
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-30 17:48:41 +0200

    COMPILAZIONE CARD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,14 @@
         <img src="{{$item->immagine}}" class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">{{$item->nome}}</h5>
-        <p class="card-text">Birra {{$item->tipologia}} di colore {{$item->colorazione}} prodotta in {{$item->zona_produzione}} fin dal {{$item->prodotta_dal}}</p>
-        <p class="card-text">{{$item->prezzo}}</p>
+        <p class="card-text">Birra {{$item->tipologia}} di colore {{$item->colorazione}} prodotta in {{$item->zona_produzione}} fin dal {{$item->prodotta_dal}} con gradazione {{$item->gradazione}}°</p>
+       
+        <p class="card-text">Prezzo: {{$item->prezzo}} €</p>
+        <p class="card-text">Dimensioni: {{$item->litraggio}} CC</p>
+      
+        
 
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Maggiori info</a>
         </div>
     </div>
 @endforeach
